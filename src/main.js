2e4aa9b33a0032f10d64933670637660632db120
surze <angpaono1@hotmail.com>
2021-04-15 21:39:28 +0700

    62130500086: [Globalregister] Footer

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import Footer from './components/Footer.vue'
 
 createApp(App).use(router).mount('#app')
+const footer = createApp(Footer)
+footer.component('component-footer', Footer)
+footer.use(router).mount('#footer')
