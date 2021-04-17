8fe9e99c2dd11acb9679570db3e82c61bb4f4834
surze <angpaono1@hotmail.com>
2021-04-17 21:26:14 +0700

    finish globalregister

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 import Footer from './components/shop-ui/Footer.vue'
+import Usercart from './components/cart/Usercart.vue'
 
 const app = createApp(App)
 app.component('footer-component', Footer)
+app.component('cart-component',Usercart)
 app.use(router).mount('#app')
