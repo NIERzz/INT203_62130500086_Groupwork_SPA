7591474e40463a4342c384f1d4df4c2d7a4fe257
surze <angpaono1@hotmail.com>
2021-04-17 21:28:32 +0700

    finish use local/global register

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,24 @@
   <div class="home">
     <Nav></Nav>
     <Itempromo></Itempromo>
+    <component-footer/>
+    <Products></Products>
+    <footer-component></footer-component>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import Nav from '@/components/Nav.vue'
-import Itempromo from '@/components/Itempromo.vue'
+import Nav from '@/components/shop-ui/Nav.vue'
+import Itempromo from '@/components/shop-ui/Itempromo.vue'
+import Products from '@/components/store/Products.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    Nav,Itempromo
+    Nav,Itempromo,Products
   }
 }
 </script>
