07bc35d1899ee29cedd17aad5949388111e74917
surze <angpaono1@hotmail.com>
2021-04-17 21:23:40 +0700

    user view products and add it to cart [fetch in component]

---
+++ src/views/Store.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="store">
+<Products></Products>
+<footer-component></footer-component>
+</div>
+</template>
+
+<script>
+import Products from '@/components/store/Products.vue'
+
+export default {
+  name: 'Store',
+  components: {
+    Products    
+  }
+ 
+
+}
+</script>
