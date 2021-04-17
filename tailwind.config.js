da620daa6fcfa71b7bac3d9ed08f8c001e0a48da
surze <angpaono1@hotmail.com>
2021-04-17 21:29:06 +0700

    little edit > return to default

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   variants: {
     extend: {},
