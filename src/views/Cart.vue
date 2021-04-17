11cf6034f7c4f9b649e608e316186a6b9ddd2a5c
surze <angpaono1@hotmail.com>
2021-04-17 21:22:54 +0700

    userview their cart

---
+++ src/views/Cart.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="cart">
+<cart-component></cart-component>
+<footer-component></footer-component>
+</div>
+</template>
+
+<script>
+import Usercart from '@/components/cart/Usercart.vue'
+
+export default {
+  name: 'Cart',
+  components: {
+      Usercart
+  }
+  }
+ 
+
+
+</script>
